44dd25e1759b91605210c1021fb765d3d99e1ce6
Alex Al-Saffar <programmer@usa.com>
2026-04-09 06:55:50 +0000

    feat: update birthday message date for Ruby in preprogram

--- src/machineconfig/scripts/python/helpers/helpers_croshell/croshell_impl.py
+++ src/machineconfig/scripts/python/helpers/helpers_croshell/croshell_impl.py
@@ -179,7 +179,7 @@ def _build_preprogram() -> str:
             print_header()
             from datetime import date
             todays_date = date.today().strftime("%Y-%m-%d")
-            if todays_date == "2026-04-09":
+            if todays_date == "2026-06-14":
                 print_logo("Happy Birthday, Ruby! 🎉")
             else:
                 # print(todays_date)
